18a2cfbf1dd4c7ba69f56c0a3024742fe2796f1c
nguyenhoainam2707 <nguyennamyp54@gmail.com>
2025-07-12 11:21:04 +0700

    update 12072025 11:20

--- src/main.cpp
+++ src/main.cpp
@@ -29,22 +29,22 @@ void setup()
   Serial.printf("Flash size : %f\n", ESP.getFlashChipSize());
 
   // Uncomment to put the app in debug mode
-  // atApp_MQTT_PUB.Debug();
+  atApp_MQTT_PUB.Debug();
   // atApp_MQTT_SUB.Debug();
   // atApp_FTP.Debug();
-  // atApp_AI.Debug();
+  atApp_AI.Debug();
   // atApp_AP.Debug();
   // atApp_CP.Debug();
   // atApp_Database.Debug();
-  atApp_DI.Debug();
+  // atApp_DI.Debug();
   // atApp_GPS.Debug();
   // atApp_LED.Debug();
   // atApp_MB.Debug();
   // atApp_PWM.Debug();
   // Uncomment to put the service in debug mode
-  // atService_EG800K.Debug();
-  // atService_ADS1115.Debug();
-  atService_PCF8574.Debug();
+  atService_EG800K.Debug();
+  atService_ADS1115.Debug();
+  // atService_PCF8574.Debug();
   // atService_Modbus.Debug();
 
   // --------------------------------------------Tasks table--------------------------------------------//
@@ -52,12 +52,12 @@ void setup()
   // || Creation  ||  Task function ||  PC name ||  Heap size ||  Parameter ||  Priority  ||  Task handle ||  CoreID  ||
   // xTaskCreatePinnedToCore(atApp_Database_Task_Func, "atApp_Database_Application", 2048, NULL, 1, &Task_atApp_Database, 1);
   // xTaskCreatePinnedToCore(atApp_CP_Task_Func, "atApp_CP_Application", 2048, NULL, 1, &Task_atApp_CP, 1);
-  // xTaskCreatePinnedToCore(atApp_MQTT_PUB_Task_Func, "atApp_MQTT_PUB_Application", 2048, NULL, 1, &Task_atApp_MQTT_PUB, 1);
+  xTaskCreatePinnedToCore(atApp_MQTT_PUB_Task_Func, "atApp_MQTT_PUB_Application", 2048, NULL, 1, &Task_atApp_MQTT_PUB, 1);
   // xTaskCreatePinnedToCore(atApp_MQTT_SUB_Task_Func, "atApp_MQTT_SUB_Application", 2048, NULL, 1, &Task_atApp_MQTT_SUB, 1);
   // xTaskCreatePinnedToCore(atApp_FTP_Task_Func, "atApp_FTP_Application", 2048, NULL, 1, &Task_atApp_FTP, 1);
   // xTaskCreatePinnedToCore(atApp_AP_Task_Func, "atApp_AP_Application", 2048, NULL, 1, &Task_atApp_AP, 1);
-  // xTaskCreatePinnedToCore(atApp_AI_Task_Func, "atApp_AI_Application", 2048, NULL, 1, &Task_atApp_AI, 1);
-  xTaskCreatePinnedToCore(atApp_DI_Task_Func, "atApp_DI_Application", 2048, NULL, 1, &Task_atApp_DI, 1);
+  xTaskCreatePinnedToCore(atApp_AI_Task_Func, "atApp_AI_Application", 2048, NULL, 1, &Task_atApp_AI, 1);
+  // xTaskCreatePinnedToCore(atApp_DI_Task_Func, "atApp_DI_Application", 2048, NULL, 1, &Task_atApp_DI, 1);
   // xTaskCreatePinnedToCore(atApp_GPS_Task_Func, "atApp_GPS_Application", 2048, NULL, 1, &Task_atApp_GPS, 1);
   // xTaskCreatePinnedToCore(atApp_LED_Task_Func, "atApp_LED_Application", 2048, NULL, 1, &Task_atApp_LED, 1);
   // xTaskCreatePinnedToCore(atApp_MB_Task_Func, "atApp_MB_Application", 2048, NULL, 1, &Task_atApp_MB, 1);
